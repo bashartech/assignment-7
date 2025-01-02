cc407f92d6c78f4cc68ee687a170217b68dc25d6
Bashar Sheikh <144168218+bashartech@users.noreply.github.com>
2025-01-03 01:45:37 +0500

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -8,28 +8,7 @@ export default function Header() {
   const cartCount = cartItems.length;
   return (
     <div>
-      {/* <nav className="bg-white shadow-md">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between h-16">
-            <div className="flex">
-              <Link href="/" className="flex-shrink-0 flex items-center">
-                <span className="text-2xl font-bold text-indigo-600">BT Store</span>
-              </Link>
-            </div>
-            <div className="flex items-center">
-              <Link href="/" className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-indigo-600 hover:bg-indigo-50 transition duration-300">Home</Link>
-              <Link href="/server-side" className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-indigo-600 hover:bg-indigo-50 transition duration-300">Server Side</Link>
-              <Link href="/fetchApi" className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-indigo-600 hover:bg-indigo-50 transition duration-300">Client Side</Link>
-            </div>
-            <Link href={`/fetchApi/product`} className='flex justify-center items-center'>
-            <div className="flex justify-center items-center">
-            <i className='bx bxs-bell text-xl cursor-pointer'></i>
-            <p id="icon" className="cursor-pointer">{cartCount}</p>
-            </div>
-            </Link>
-          </div>
-        </div>
-      </nav> */}
+     
         <nav className={`fixed w-full z-10 transition-all duration-300`}>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16">

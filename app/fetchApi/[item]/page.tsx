c6f1b0abf7e168572699e98189eabb9c80eacfaf
Bashar Sheikh <144168218+bashartech@users.noreply.github.com>
2025-01-03 01:42:14 +0500

    Update page.tsx

--- app/fetchApi/[item]/page.tsx
+++ app/fetchApi/[item]/page.tsx
@@ -34,30 +34,6 @@ export default function Section10({ params, }: { params: { item: string } }) {
   const handleButton = () => {
     alert("Product is added in cart")
   }
- 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       setLoading(true);
-  //       const response = await fetch(
-  //         `https://fakestoreapi.com/products/${params.item}`
-  //       );
-
-  //       if (!response.ok) {
-  //         throw new Error("Failed to fetch product data.");
-  //       }
-
-  //       const productData: ITodo = await response.json();
-  //       setData(productData);
-  //     } catch (err: any) {
-  //       setError(err.message || "An unexpected error occurred.");
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, [params.item]);
 
   if (loading) {
     return (
@@ -70,9 +46,7 @@ export default function Section10({ params, }: { params: { item: string } }) {
       </div>
     );
   }
-  // if (error) {
-  //   return <div className="text-center text-red-500">{error}</div>;
-  // }
+
 
   if (!data) {
     return (
@@ -86,124 +60,7 @@ export default function Section10({ params, }: { params: { item: string } }) {
     );
   }
   return (
-  //   <div className="">
-  //     <link
-  //       href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
-  //       rel="stylesheet"
-  //     />
-  //     <Header />
-  //     {/* {data.length === 0 ? (
-  //       <p className="text-center text-lg text-gray-600">Loading...</p>
-  //     ) : (
-  //       data.map((item, index) => ( */}
-  //         <div >
-  //           <div className="lg:w-screen mt-10 flex overflow-hidden justify-center items-center lg:h-[92px]">
-  //             <div className="lg:w-[1049px]  mb-5  md:h-full flex  items-center">
-  //               <h1 className="text-[16px]font-bold text-[#252B42]">Home/</h1>
-  //               <i className="bx bx-chevron-right text-2xl text-[#737373]"></i>
-  //               <h1>Shop</h1>
-  //             </div>
-  //           </div>
-  //           <div className="lg:w-screen  lg:h-[598px] flex justify-center items-center">
-  //             <div className="lg:w-[1050px]  w-[400px] lg:flex lg:h-[550px] gap-10 ">
-  //               <div className="left lg:w-1/2">
-  //                 <div className="lg:w-[506px] md:h-[450px] ">
-  //                   <Image
-  //                     src={data.image}
-  //                     alt="hero image"
-  //                     width={506}
-  //                     height={450}
-  //                   ></Image>
-  //                 </div>
-  //                 <div className="w-full mt-2 h-[75px] flex gap-2">
-  //                   <Image
-  //                     src={data.image}
-  //                     alt="hero image"
-  //                     width={100}
-  //                     height={75}
-  //                   ></Image>
-  //                   <Image
-  //                     src={data.image}
-  //                     alt="hero image"
-  //                     width={100}
-  //                     height={75}
-  //                   ></Image>
-  //                 </div>
-  //               </div>
-  //               <div className="right flex justify-center  pt-5 lg:w-1/2">
-  //                 <div className="lg:w-[510px] p-8  w-[400px] flex flex-col  gap-5 lg:h-[471px]">
-  //                   <h1 className="text-[20px] font-bold">{data.title}</h1>
-  //                   <div className=" flex gap-3">
-  //                     <div className="icon  text-[#F3CD03]">
-  //                       <i className="bx bxs-star text-lg"></i>
-  //                       <i className="bx bxs-star text-lg"></i>
-  //                       <i className="bx bxs-star text-lg"></i>
-  //                       <i className="bx bxs-star text-lg"></i>
-  //                       <i className="bx bx-star text-lg"></i>
-  //                     </div>
-  //                     <h1
-  //                       className="font-bold
-  //               "
-  //                     >
-  //                       10 Reviews
-  //                     </h1>
-  //                   </div>
-  //                   <p className="font-bold text-[24px]">${data.price}</p>
-  //                   <div>
-  //                     <span className="font-bold">Availability :</span>
-  //                     <span className="font-bold text-[#23A6F0]">
-  //                       In Stock{" "}
-  //                     </span>
-  //                   </div>
-  //                   <p>{data.description}</p>
-  //                   <div className="line w-[445px] bg-[#BDBDBD] h-[1px]"></div>
-  //                   <div className="colors flex gap-2 w-[150px] h-[30px]">
-  //                     <div
-  //                       className="w-[30px] bg-[#23A6F0]
-  //  h-[30px] rounded-full"
-  //                     ></div>
-  //                     <div className="w-[30px] bg-[#2DC071]  h-[30px]  rounded-full"></div>
-  //                     <div className="w-[30px] bg-[#E77C40] h-[30px] rounded-full"></div>
-  //                     <div className="w-[30px] bg-[#252B42] h-[30px] rounded-full"></div>
-  //                     <div className="w-[30px] bg-[] h-[30px] rounded-full"></div>
-  //                   </div>
-  //                   <div className="icon flex gap-3">
-  //                     <Button
-  //                       className='font-bold bg-[#23A6F0]  w-[148px] h-[44px] ">
-  //                 '
-  //                       onClick={(e) => {
-  //                         e.preventDefault();
-  //                         addToCart({
-  //                           id: data.id,
-  //                           title: data.title,
-  //                           price: data.price,
-  //                           quantity: 1,
-  //                           image: data.image,
-  //                           description: data.description,
-  //                         });
-  //                       }}
-  //                     >
-  //                       Select Options
-  //                     </Button>
-  //                     <div className=" flex gap-4 justify-center items-center ">
-  //                       <i className="bx bx-cart "></i>
-  //                       <i className="bx bx-heart "></i>
-  //                       <Image
-  //                         src={data.image}
-  //                         alt="hero image"
-  //                         width={20}
-  //                         height={20}
-  //                       ></Image>
-  //                     </div>
-  //                   </div>
-  //                 </div>
-  //               </div>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       {/* ))
-  //     )} */}
-  //   </div>
+
   <div className="bg-gradient-to-br from-turquoise-100 to-lavender-200 min-h-screen">
       <link
         href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
